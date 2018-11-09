e07dd9b1b7eb304d6df249b09a4ca7c321031a09
slamdf150xc <slamdf150xc@gmail.com>
2018-11-09 12:00:38 -0600

    Update Resume-EPVUser.ps1

--- Resume-EPVUser.ps1
+++ Resume-EPVUser.ps1
@@ -89,6 +89,7 @@ $resume = EPV-ActivateUser $userToResume
 
 If ($resume.Suspended -eq $false) {
 	Write-Host "$userToResume was successfully resumed!" -ForegroundColor Green
+	EPV-Logoff
 }
 
 ########################### END SCRIPT ###################################################
